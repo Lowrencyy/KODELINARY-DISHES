68839ef3d985a0ff268b271f22db984188ed2bca
Lowrencyy <Lowrencyy@gmail.com>
2023-11-01 04:27:41 +0800

    with search bar unfinish

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Category from './components/Category'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer'
+import Search from './components/Search/Search';
+
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Header/>
+      <Search/>
       <Category/>
       <Pages/>
       <Footer/>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import styled from "styled-components"
+import { useState } from "react"
+import { FaSearch } from "react-icons/fa"
+
 
 function Search() {
   return (
-    <div>Search</div>
+    <FormStyle>
+      <div>
+    
+      <input type="text"/>
+      </div>
+    </FormStyle>
   )
 }
 
+const FormStyle = styled.div`
+
+margin:0rem;
+padding-top:40px;
+div{
+  width:100%;
+  position:relative;
+}
+
+input{
+  border:none;
+  background:linear-gradient(35deg,#494949 , #313131);
+  font-size:1.5rem;
+  color:white;
+  padding:0.6rem 2rem;
+  border:none;
+  border-radius:1rem;
+  outline:none;
+  width:50%;
+  margin-left:23.5%;
+}
+
+svg{
+  position:absolute;
+  top:50%;
+  left:0%;
+  transform:translate(100%,-50%);
+  color:white;
+
+}
+
+`
 export default Search
+
+
+last do 1:33 
